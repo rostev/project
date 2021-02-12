d692c9503fd6e06bbbde6f1e2a4d5b91a8f2ce66
Ruprecht <rostevWork@mail.ru>
2021-02-12 14:13:08 +1000

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+"use strict";
+/*
+
+/!*
+const number = 7.4;
+
+console.log(number / -0);
+console.log("=======================");
+console.log("string" * 9);
+
+const string = "Doris";
+console.log("=======================");
+console.log(string);
+
+const bool = true;
+console.log("=======================");
+console.log(bool);
+
+/!*console.log("=======================");
+console.log(value);*!/
+
+let und;
+console.log("=======================");
+console.log(und);
+*!/
+
+/!*
+const obj = {
+    firstName: "Ruprecht",
+    lastName: "Freeman",
+    age: 40,
+    isMarried: true
+};
+
+console.log("=======================");
+console.log(obj.firstName);
+
+/!*
+console.log("=======================");
+console.log(obj["firstName"]);*!/
+
+const array = [1, 2, 3, 4, 5,];
+
+console.log("=======================");
+console.log(array[0]);*!/
+
+/!*
+const result = confirm("Are you here?");
+console.log(result);*!/
+
+/!*
+const answer = prompt("What is your name?", "Boris");
+console.log(+answer + 5);*!/
+
+const answers = [];
+
+answers[0] = prompt("What is your first name?", "");
+answers[1] = prompt("What is your last name?", "");
+answers[2] = +prompt("How old are you ?", "");
+
+console.log(typeof (answers));*/
+
+/*
+const category = "toys";
+
+console.log(`https://someurl.com/${category}`);
+console.log("=======================");
+
+const user = "Boris";
+alert(`Hello, ${user}`);*/
+
+console.log("array" + " - object");
+console.log("=======================");
